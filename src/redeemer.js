c90286afdd3dce125694c7830773b3a39fae9c02
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-02-11 22:07:23 -0300

    Added logging and fixes Now filters out the failed codes, changed how it redeems codes

--- src/redeemer.js
+++ src/redeemer.js
@@ -2,6 +2,7 @@ const puppeteer = require("puppeteer");
 const { createSpinner } = require("nanospinner");
 const { readFileSync } = require("fs");
 const path = require("path");
+const { addLog, readLog } = require("./logger");
 
 const codesPath = process.pkg
   ? path.join(path.dirname(process.execPath), "config", "codes.txt")
@@ -13,12 +14,18 @@ let codes = readFileSync(codesPath, {
   .split("\n")
   .filter((x) => x !== "");
 
-module.exports = async function redeemer(path, cookies) {
+module.exports = async function redeemer(cookies, path) {
   if (!codes[0]) {
     throw new Error(
       "There are no codes to redeem, make sure that codes.txt is not empty"
     );
   }
+
+  const loginSpinner = createSpinner("Trying to login").start();
+  const browser = await puppeteer.launch({
+    headless: "new",
+    executablePath: path,
+  });
 
   cookies = cookies.map((x) => {
     if (x.partitionKey === null) {
@@ -30,36 +37,32 @@ module.exports = async function redeemer(path, cookies) {
     return x;
   });
 
-  const loginSpinner = createSpinner("Trying to login").start();
-  const browser = await puppeteer.launch({
-    headless: "new",
-    executablePath: path,
-  });
   const [page] = await browser.pages();
-  await page.setViewport({
-    width: 1920,
-    height: 1080,
-  });
+  await page.setExtraHTTPHeaders({ DNT: "1" });
 
   await page.setCookie(...cookies);
-  await page.goto("https://www.warframe.com/");
-
   await page.setRequestInterception(true);
 
   page.on("request", (req) => {
     if (
       req.resourceType() == "font" ||
       req.resourceType() == "image" ||
-      req.resourceType() == "stylesheet"
+      req.resourceType() == "stylesheet" ||
+      req.resourceType() == "media"
     ) {
       req.abort();
     } else {
       req.continue();
     }
   });
+  await page.goto("https://www.warframe.com/promocode", {
+    waitUntil: "domcontentloaded",
+  });
   let username = await page
     .waitForSelector(".HeaderUserProfile-nameText")
     .then((el) => el.evaluate((x) => x.textContent));
+
+  await new Promise((resolve) => setTimeout(resolve, 250));
 
   if (username === "--") {
     browser.close();
@@ -72,15 +75,19 @@ module.exports = async function redeemer(path, cookies) {
     text: `Successfully logged in as ${username}`,
   });
 
+  const log = readLog();
+  codes = codes.filter((x) => !log.find((y) => y?.code === x));
+  console.log(`Found ${codes.length} codes to redeem\n`);
+
   for (let code of codes) {
-    await page.goto("https://www.warframe.com/promocode");
+    console.time("It took");
+    await page.waitForSelector("#promocode_input").then((e) => e.type(code));
     let codeSpinner = createSpinner(`Trying to redeem ${code}`).start();
 
-    await page.waitForSelector("#promocode_input").then((e) => e.type(code));
     await Promise.all([
       page.waitForNavigation({
         waitUntil: "domcontentloaded",
-        timeout: 60_000,
+        timeout: 20_000,
       }),
       page.click("#btnSubmit"),
     ]);
@@ -95,21 +102,27 @@ module.exports = async function redeemer(path, cookies) {
         text: `${code} redeemed successfully\n`,
       });
     } else {
-      let message = await page.$eval(
-        ".alert-danger li",
-        (el) => el.textContent
-      );
+      let alert = await page.waitForSelector(".alert-danger li");
+      let message = await alert.evaluate((el) => el.textContent);
 
-      codeSpinner.error({
-        text: message.includes("you already redeemed a code")
-          ? `Already claimed ${code}`
-          : `Failed to redeem ${code}\n`,
-      });
+      if (message.includes("you already redeemed a code")) {
+        codeSpinner.error({
+          text: `Already claimed ${code}`,
+        });
+        addLog(code, "Already claimed");
+      } else {
+        codeSpinner.error({
+          text: `Failed to redeem ${code}`,
+        });
+        addLog(code, "Failed to redeem possibly non existent or expired code");
+      }
     }
 
+    console.timeEnd("It took");
     console.log(`${codes.length - 1} Codes remaining\n`);
 
     codes = codes.filter((x) => x !== code);
+    await page.reload();
   }
   await browser.close();
 };
