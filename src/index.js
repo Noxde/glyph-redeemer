31b07916a2cd3dc03a054d569384d4855abcfb5a
Mateo Tesorieri <70549780+Noxde@users.noreply.github.com>
2024-05-04 17:02:02 -0300

    Added program version to be able to check it on the updater

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const path = require("path");
 const { createSpinner } = require("nanospinner");
 const readline = require("readline");
 const { VTexec } = require("open-term");
+const { isLatest, update } = require("./updater.js");
 
+const version = "1.2.1"; // Lower version number for testing purposes this is actually 1.2.3 with changes
 // If its not windows and not running on a terminal then exit
 if (!process.stdout.isTTY && !process.env.APPDATA) {
   VTexec("echo 'Please run the script from the terminal.'");
@@ -16,24 +18,10 @@ const cookiesPath = process.pkg
   : "../config/cookies.json";
 
 (async function () {
-  let cookies;
-  try {
-    cookies = require(cookiesPath);
-  } catch (err) {
-    console.error(
-      "Could not find the cookies file make sure its in the config folder and the file is not empty."
-    );
-    if (process.env.APPDATA) {
-      return readline
-        .createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        })
-        .question("Press enter to close the program...", (ans) =>
-          process.exit(0)
-        );
-    }
-    process.exit(0);
+  const isUpdated = await isLatest(version);
+
+  if (!isUpdated.isLatest) {
+    await update(isUpdated.assets);
   }
 
   const chromiumSpinner = createSpinner("Installing chromium").start();
