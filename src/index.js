bb93e61913f5588a3119b9b452176a659bbce4d2
Noxde <mateo.tesorieri@hotmail.com>
2024-07-01 23:35:29 -0300

    Version is now 1.3.0, moved some stuff - Ordered the require statements - The cookies check remained on the index - The codes are fetched on the index instead of redeemer.js - It fetches the codes before trying to login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-const redeemer = require("./redeemer.js");
-const { installChromium, removeChromium } = require("./installer.js");
 const path = require("path");
-const { createSpinner } = require("nanospinner");
-const readline = require("readline");
-const { VTexec } = require("open-term");
-const { isLatest, update } = require("./updater.js");
 
-const version = "1.2.1"; // Lower version number for testing purposes this is actually 1.2.3 with changes
+const { createSpinner } = require("nanospinner");
+const { VTexec } = require("open-term");
+
+const { installChromium, removeChromium } = require("./installer.js");
+const { isLatest, update } = require("./updater.js");
+const exitProgram = require("./exitProgram.js");
+const { readCodeLog } = require("./logger");
+const getCodes = require("../codeUpdater");
+const redeemer = require("./redeemer.js");
+
+const version = "1.3.0"; // Lower version number for testing purposes this is actually 1.2.3 with changes
 // If its not windows and not running on a terminal then exit
 if (!process.stdout.isTTY && !process.env.APPDATA) {
   VTexec("echo 'Please run the script from the terminal.'");
@@ -24,7 +28,31 @@ const cookiesPath = process.pkg
     await update(isUpdated.assets);
   }
 
-  let cookies = checkCookies();
+  let cookies;
+  try {
+    cookies = require(cookiesPath);
+  } catch (err) {
+    console.error(
+      "Could not find the cookies file make sure its in the config folder and the file is not empty."
+    );
+    return exitProgram();
+  }
+
+  const log = readCodeLog();
+  const codesSpinner = createSpinner("Fetching codes").start();
+  let codes = await getCodes();
+  codesSpinner.success({
+    text: "Codes fetched",
+  });
+
+  codes = codes
+    .map((x) => x.code)
+    .filter((x) => !log.find((y) => y?.code === x));
+  if (codes.length == 0) {
+    console.log("There are no new codes to redeem.");
+    return exitProgram();
+  }
+  console.log(`Found ${codes.length} new codes to redeem\n`);
 
   const chromiumSpinner = createSpinner("Installing chromium").start();
   const chromiumPath = await installChromium();
@@ -33,42 +61,10 @@ const cookiesPath = process.pkg
   });
 
   console.time("No codes left to redeem. Time taken");
-  await redeemer(cookies, chromiumPath);
+  await redeemer(codes, cookies, chromiumPath);
   console.timeEnd("No codes left to redeem. Time taken");
 
   await removeChromium();
 
-  if (process.platform === "win32") {
-    readline
-      .createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      })
-      .question("Press enter to close the program...", (ans) =>
-        process.exit(0)
-      );
-  }
+  exitProgram();
 })();
-
-function checkCookies() {
-  let cookies;
-  try {
-    cookies = require(cookiesPath);
-    return cookies;
-  } catch (err) {
-    console.error(
-      "Could not find the cookies file make sure its in the config folder and the file is not empty."
-    );
-    if (process.env.APPDATA) {
-      return readline
-        .createInterface({
-          input: process.stdin,
-          output: process.stdout,
-        })
-        .question("Press enter to close the program...", (ans) =>
-          process.exit(0)
-        );
-    }
-    process.exit(0);
-  }
-}
