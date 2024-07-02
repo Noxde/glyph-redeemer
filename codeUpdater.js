1bdbc84d83155964e34b678ed97ed317150ba702
Noxde <mateo.tesorieri@hotmail.com>
2024-07-01 23:21:14 -0300

    Replaced fetch with axios

--- codeUpdater.js
+++ codeUpdater.js
@@ -1,28 +1,25 @@
 const cheerio = require("cheerio");
+const axios = require("axios").default;
 
 /**
  * Thanks voltage for updating the list of codes
  * Link to his profile on the warframe forums https://forums.warframe.com/profile/1419671-voltage/
  */
-module.exports = function getCodes() {
-  const codes = fetch(
+module.exports = async function getCodes() {
+  const { data } = await axios.get(
     "https://forums.warframe.com/topic/992008-free-promocodes-and-glyphs-all-platforms/"
-  )
-    .then((res) => res.text())
-    .then((data) => {
-      const $ = cheerio.load(data);
-      const table = $(".ipsSpoiler_contents").first();
-      const codes = table.find("ol > li");
-      const jsonCodes = codes.map((_, el) => {
-        const element = $(el);
-        return {
-          code: element.find("a").first().text().trim(),
-          img: element.find("img").attr("src"),
-        };
-      });
+  );
 
-      return jsonCodes.get();
-    });
+  const $ = cheerio.load(data);
+  const table = $(".ipsSpoiler_contents").first();
+  const codes = table.find("ol > li");
+  const jsonCodes = codes.map((_, el) => {
+    const element = $(el);
+    return {
+      code: element.find("a").first().text().trim(),
+      img: element.find("img").attr("src"),
+    };
+  });
 
   return codes;
 };
