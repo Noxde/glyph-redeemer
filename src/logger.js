6c83a64aaaba114bb495956cb48193637bf8cde6
Noxde <mateo.tesorieri@hotmail.com>
2024-07-01 23:40:44 -0300

    Now there is a log for redeemed codes and one for errors

--- src/logger.js
+++ src/logger.js
@@ -1,29 +1,50 @@
-const winston = require("winston");
+const {
+  createLogger,
+  format: { timestamp, combine, json },
+  transports,
+} = require("winston");
 const { readFileSync, existsSync } = require("fs");
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: "./errors.log" })],
+const errorLogger = createLogger({
+  level: "error",
+  format: combine(timestamp(), json()),
+  transports: [new transports.File({ filename: "./logs/errors.log" })],
 });
 
-module.exports.addLog = function (code, message) {
-  logger.log({
-    level: "error",
-    code,
-    message,
-  });
-};
+const codeLogger = createLogger({
+  level: "info",
+  format: combine(timestamp(), json()),
+  transports: [new transports.File({ filename: "./logs/redeemed.log" })],
+});
 
-module.exports.readLog = function () {
-  if (!existsSync("./errors.log")) {
+function logCode(code, log) {
+  codeLogger.info({
+    message: log,
+    code,
+  });
+}
+
+function logError(err) {
+  errorLogger.error({
+    message: err,
+  });
+}
+
+function readCodeLog() {
+  if (!existsSync("./logs/redeemed.log")) {
     return [];
   }
 
-  const json = readFileSync("./errors.log", { encoding: "utf-8" })
+  const json = readFileSync("./logs/redeemed.log", { encoding: "utf-8" })
     .split("\n")
     .filter(Boolean)
     .map((x) => JSON.parse(x));
 
   return json;
+}
+
+module.exports = {
+  logCode,
+  logError,
+  readCodeLog,
 };
