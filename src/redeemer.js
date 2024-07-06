b17c9a9f09ff611d7b9e8974d455bbe924892488
Noxde <mateo.tesorieri@hotmail.com>
2024-07-06 17:00:39 -0300

    Set user agent and viewport

--- src/redeemer.js
+++ src/redeemer.js
@@ -22,6 +22,13 @@ module.exports = async function redeemer(codes, cookies, path) {
   });
 
   const [page] = await browser.pages();
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+  );
+  await page.setViewport({
+    height: 1080,
+    width: 1920,
+  });
 
   await page.setCookie(...cookies);
 
