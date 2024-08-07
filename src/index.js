5eed0a560d6219d0f51aff8ac73355c0cf3c6902
Noxde <mateo.tesorieri@hotmail.com>
2024-08-07 12:47:58 -0300

    Added try catch, bumped version number

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const { VTexec } = require("open-term");
 const { installChromium, removeChromium } = require("./installer.js");
 const { isLatest, update } = require("./updater.js");
 const exitProgram = require("./exitProgram.js");
-const { readCodeLog } = require("./logger");
+const { readCodeLog, logError } = require("./logger");
 const getCodes = require("../codeUpdater");
 const redeemer = require("./redeemer.js");
 
-const version = "1.3.2";
+const version = "1.3.3";
 // If its not windows and not running on a terminal then exit
 if (!process.stdout.isTTY && !process.env.APPDATA) {
   VTexec("echo 'Please run the script from the terminal.'");
@@ -22,49 +22,56 @@ const cookiesPath = process.pkg
   : "../config/cookies.json";
 
 (async function () {
-  const isUpdated = await isLatest(version);
-
-  if (!isUpdated.isLatest) {
-    await update(isUpdated.assets);
-  }
-
-  let cookies;
   try {
-    cookies = require(cookiesPath);
-  } catch (err) {
-    console.error(
-      "Could not find the cookies file make sure its in the config folder and the file is not empty."
-    );
-    return exitProgram();
+    const isUpdated = await isLatest(version);
+
+    if (!isUpdated.isLatest) {
+      await update(isUpdated.assets);
+    }
+
+    let cookies;
+    try {
+      cookies = require(cookiesPath);
+    } catch (err) {
+      console.error(
+        "Could not find the cookies file make sure its in the config folder and the file is not empty."
+      );
+      return exitProgram();
+    }
+
+    const log = readCodeLog();
+    const codesSpinner = createSpinner("Fetching codes").start();
+    let codes = await getCodes();
+    codesSpinner.success({
+      text: "Codes fetched",
+    });
+
+    codes = codes
+      .map((x) => x.code)
+      .filter((x) => !log.find((y) => y?.code === x));
+    if (codes.length == 0) {
+      console.log("There are no new codes to redeem.");
+      return exitProgram();
+    }
+    console.log(`Found ${codes.length} new codes to redeem\n`);
+
+    const chromiumSpinner = createSpinner("Installing chromium").start();
+    const chromiumPath = await installChromium();
+    chromiumSpinner.success({
+      text: "Chromium installed",
+    });
+
+    console.time("No codes left to redeem. Time taken");
+    await redeemer(codes, cookies, chromiumPath);
+    console.timeEnd("No codes left to redeem. Time taken");
+
+    await removeChromium();
+
+    exitProgram();
+  } catch (error) {
+    console.log(`\nError: ${error.message}`);
+    logError(error.message);
+
+    process.exit(1);
   }
-
-  const log = readCodeLog();
-  const codesSpinner = createSpinner("Fetching codes").start();
-  let codes = await getCodes();
-  codesSpinner.success({
-    text: "Codes fetched",
-  });
-
-  codes = codes
-    .map((x) => x.code)
-    .filter((x) => !log.find((y) => y?.code === x));
-  if (codes.length == 0) {
-    console.log("There are no new codes to redeem.");
-    return exitProgram();
-  }
-  console.log(`Found ${codes.length} new codes to redeem\n`);
-
-  const chromiumSpinner = createSpinner("Installing chromium").start();
-  const chromiumPath = await installChromium();
-  chromiumSpinner.success({
-    text: "Chromium installed",
-  });
-
-  console.time("No codes left to redeem. Time taken");
-  await redeemer(codes, cookies, chromiumPath);
-  console.timeEnd("No codes left to redeem. Time taken");
-
-  await removeChromium();
-
-  exitProgram();
 })();
